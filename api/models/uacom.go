84f83bcc07bc44b42731116eff6d74d0718d2748
MCTIC <alexandre.ps1123@gmail.com>
2020-03-09 13:47:17 -0300

    UACOM: funcao UPDATE agora utiliza o metodo Exec para fazer UPDATE no banco de dados

--- api/models/uacom.go
+++ api/models/uacom.go
@@ -56,10 +56,7 @@ func (uacom *Uacom) FindAllUacom(db *gorm.DB) (*[]Uacom, error) {
 func (uacom *Uacom) UpdateUacom(db *gorm.DB, codIbge uint64, data string) (*Uacom, error) {
 
 	//	Permite a atualizacao dos campos indicados
-	err := db.Debug().Model(&Uacom{}).Where("cod_ibge = ? AND data = ?", codIbge, data).Updates(
-		Uacom{
-			Titulo: uacom.Titulo,
-			Relato: uacom.Relato}).Error
+	err := db.Debug().Exec("UPDATE uacom SET titulo = ?, relato = ? WHERE cod_ibge = ? AND data = ?", uacom.Titulo, uacom.Relato, codIbge, data)..Error
 
 	if db.Error != nil {
 		return &Uacom{}, db.Error

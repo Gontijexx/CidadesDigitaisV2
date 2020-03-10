391edf541e9bfb7c9b606f0784cb5bf15dc5e1b1
dimisworkhub <dimitrius_cardoso@hotmail.com>
2020-03-10 15:07:08 -0300

    join dos itens

--- main.go
+++ main.go
@@ -5,3 +5,9 @@ import "CidadesDigitaisV2/api"
 func main() {
 	api.Run()
 }
+
+//join do lote_itens e itens
+// select * from lote_itens
+// inner join itens on lote_itens.cod_item = itens.cod_item and lote_itens.cod_tipo_item = itens.cod_tipo_item
+
+// -- Código do Item, Tipo de Item e Descrição do item	

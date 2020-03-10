46de965fb3b8081b9794051496188efaa3526d69
MCTIC <alexandre.ps1123@gmail.com>
2020-03-10 09:09:52 -0300

    ITENSFATURA: Funcao de GetByID implementada. Funcao de UPDATE atualizada para utilizar todas as PK. Funcao de DELETE atualizada para utilizar todas as PK. E, GetAll nao utiliza mais PK para fazer busca. Todas as funcoes foram testadas e estao funcionando.

--- api/models/itensFatura.go
+++ api/models/itensFatura.go
@@ -8,7 +8,7 @@ import (
 
 /*  =========================
 	FUNCAO SALVAR ITENS FATURA NO BANCO DE DADOS
-=========================
+=========================  */
 
 func (itensFatura *ItensFatura) SaveItensFatura(db *gorm.DB) (*ItensFatura, error) {
 
@@ -20,18 +20,33 @@ func (itensFatura *ItensFatura) SaveItensFatura(db *gorm.DB) (*ItensFatura, erro
 
 	return itensFatura, nil
 }
-*/
+
+/*  =========================
+	FUNCAO LISTAR ITENS FATURA POR ID
+=========================  */
+
+func (itensFatura *ItensFatura) FindItensFaturaByID(db *gorm.DB, numNF, codIbge, idEmpenho, codItem, codTipoItem uint64) (*ItensFatura, error) {
+
+	//	Busca um elemento no banco de dados de acordo com suas chaves primarias
+	err := db.Debug().Model(ItensEmpenho{}).Where("num_nf = ? AND cod_ibge = ? AND id_empenho = ? AND cod_item = ? AND cod_tipo_item = ?", numNF, codIbge, idEmpenho, codItem, codTipoItem).Take(&itensFatura).Error
+
+	if err != nil {
+		return &ItensFatura{}, err
+	}
+
+	return itensFatura, err
+}
 
 /*  =========================
 	FUNCAO LISTAR TODAS ITENS FATURA
 =========================  */
 
-func (itensFatura *ItensFatura) FindAllItensFatura(db *gorm.DB, numNF uint64) (*[]ItensFatura, error) {
+func (itensFatura *ItensFatura) FindAllItensFatura(db *gorm.DB) (*[]ItensFatura, error) {
 
 	allItensFatura := []ItensFatura{}
 
 	// Busca todos elementos contidos no banco de dados
-	err := db.Debug().Model(&ItensFatura{}).Where("num_nf = ?", numNF).Find(&allItensFatura).Error
+	err := db.Debug().Model(&ItensFatura{}).Find(&allItensFatura).Error
 	if err != nil {
 		return &[]ItensFatura{}, err
 	}
@@ -42,13 +57,10 @@ func (itensFatura *ItensFatura) FindAllItensFatura(db *gorm.DB, numNF uint64) (*
 	FUNCAO EDITAR ITENS FATURA
 =========================  */
 
-func (itensFatura *ItensFatura) UpdateItensFatura(db *gorm.DB) (*ItensFatura, error) {
+func (itensFatura *ItensFatura) UpdateItensFatura(db *gorm.DB, numNF, codIbge, idEmpenho, codItem, codTipoItem uint64) (*ItensFatura, error) {
 
 	//	Permite a atualizacao dos campos indicados
-	err := db.Debug().Model(&ItensFatura{}).Updates(
-		ItensFatura{
-			Valor:      itensFatura.Valor,
-			Quantidade: itensFatura.Quantidade}).Error
+	err := db.Debug().Exec("UPDATE itens_fatura SET valor = ?, quantidade = ? WHERE num_nf = ? AND cod_ibge = ? AND id_empenho = ? AND cod_item = ? AND cod_tipo_item = ?", itensFatura.Valor, itensFatura.Quantidade, numNF, codIbge, idEmpenho, codItem, codTipoItem).Error
 
 	if db.Error != nil {
 		return &ItensFatura{}, db.Error
@@ -68,10 +80,10 @@ func (itensFatura *ItensFatura) UpdateItensFatura(db *gorm.DB) (*ItensFatura, er
 	FUNCAO DELETAR ITENS OTB POR ID
 =========================  */
 
-func (itensFatura *ItensFatura) DeleteItensFatura(db *gorm.DB, numNF, idEmpenho, codItem, codTipoItem uint64) (int64, error) {
+func (itensFatura *ItensFatura) DeleteItensFatura(db *gorm.DB, numNF, cod_ibge, idEmpenho, codItem, codTipoItem uint64) (int64, error) {
 
 	//	Deleta um elemento contido no banco de dados a partir de sua chave primaria
-	db = db.Debug().Model(&ItensFatura{}).Where("num_nf = ? AND id_empenho = ? AND cod_item = ? AND cod_tipo_item = ?", numNF, idEmpenho, codItem, codTipoItem).Take(&ItensFatura{}).Delete(&ItensFatura{})
+	db = db.Debug().Model(&ItensFatura{}).Where("num_nf = ? AND cod_ibge = ? AND id_empenho = ? AND cod_item = ? AND cod_tipo_item = ?", numNF, cod_ibge, idEmpenho, codItem, codTipoItem).Take(&ItensFatura{}).Delete(&ItensFatura{})
 
 	if db.Error != nil {
 		if gorm.IsRecordNotFoundError(db.Error) {

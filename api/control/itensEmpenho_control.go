ee5eb2bdc1b663b7b2f3b76c5dc968ab5ebbaaec
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-03-25 11:12:07 -0300

    ItensEmpenho: implementada a funcao de CreateItensEmpenho.

--- api/control/itensEmpenho_control.go
+++ api/control/itensEmpenho_control.go
@@ -16,6 +16,61 @@ import (
 )
 
 /*  =========================
+	FUNCAO ADICIONAR ITENS EMPENHO
+=========================  */
+
+func (server *Server) CreateItensEmpenho(w http.ResponseWriter, r *http.Request) {
+
+	//	Autorizacao de Modulo
+	err := config.AuthMod(w, r, 15001)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
+		return
+	}
+	//	O metodo ReadAll le toda a request ate encontrar algum erro, se nao encontrar erro o leitura para em EOF
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] it couldn't read the body, %v\n", err))
+		return
+	}
+
+	//	Estrutura models.ItensEmpneho{} "renomeada"
+	itensEmpenho := models.ItensEmpenho{}
+
+	//	Unmarshal analisa o JSON recebido e armazena na struct entidade referenciada (&struct)
+	err = json.Unmarshal(body, &itensEmpenho)
+
+	//	Se ocorrer algum tipo de erro retorna-se o Status 422 mais o erro ocorrido
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
+		return
+	}
+
+	if err = validation.Validator.Struct(itensEmpenho); err != nil {
+		log.Printf("[WARN] invalid information, because, %v\n", fmt.Errorf("[FATAL] validation error!, %v\n", err))
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	//	SaveItensEmpenho eh o metodo que faz a conexao com banco de dados e salva os dados recebidos
+	itensEmpenhoCreated, err := itensEmpenho.SaveItensEmpenho(server.DB)
+
+	//	Retorna um erro caso nao seja possivel salvar entidade no banco de dados
+	//	Status 500
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save in database, %v\n", formattedError))
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d/%d/%d", r.Host, r.RequestURI, itensEmpenhoCreated.IDEmpenho, itensEmpenhoCreated.CodItem, itensEmpenhoCreated.CodTipoItem))
+
+	//	Ao final retorna o Status 201 e o JSON da struct que foi criada
+	responses.JSON(w, http.StatusCreated, itensEmpenhoCreated)
+
+}
+
+/*  =========================
 	FUNCAO LISTAR ITENS EMPENHO POR ID
 =========================  */
 

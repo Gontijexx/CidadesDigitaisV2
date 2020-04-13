316c5060d0fac721eea01ca62923272f3492754e
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-04-13 16:16:33 -0300

    Home: removido as linhas de implementacao template.

--- api/control/home_control.go
+++ api/control/home_control.go
@@ -1,16 +1,13 @@
 package control
 
 import (
-	"html/template"
 	"net/http"
 
 	"CidadesDigitaisV2/api/responses"
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
-	homepageTpl := template.Must(template.New("homepage_view").ParseFiles("../Front/home.html"))
-	homepageTpl.Execute(w, nil)
 
+	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
 	return
 }

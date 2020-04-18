7af653fea137b7b14e9c73f76c1d357fff52e1bf
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-04-17 23:47:11 -0300

    LoteItens: implementada a funcao de log.

--- api/control/loteItens_control.go
+++ api/control/loteItens_control.go
@@ -1,11 +1,13 @@
 package control
 
 import (
+	"CidadesDigitaisV2/api/auth"
 	"CidadesDigitaisV2/api/config"
 	"CidadesDigitaisV2/api/models"
 	"CidadesDigitaisV2/api/responses"
 	"CidadesDigitaisV2/api/validation"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -33,7 +35,15 @@ func (server *Server) CreateLoteItens(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	loteItens := models.LoteItens{}
+	logLoteItens := models.Log{}
 
 	err = json.Unmarshal(body, &loteItens)
 	if err != nil {
@@ -41,9 +51,19 @@ func (server *Server) CreateLoteItens(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err = validation.Validator.Struct(loteItens); err != nil {
+	//	Validacao de estrutura
+	err = validation.Validator.Struct(loteItens)
+	if err != nil {
 		log.Printf("[WARN] invalid information, because, %v\n", fmt.Errorf("[FATAL] validation error!, %v\n", err))
 		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logLoteItens.LogLoteItens(server.DB, loteItens.CodLote, loteItens.CodItem, loteItens.CodTipoItem, "lote_itens", "i", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
 		return
 	}
 
@@ -58,7 +78,6 @@ func (server *Server) CreateLoteItens(w http.ResponseWriter, r *http.Request) {
 
 	//	Ao final retorna o Status 201 e o JSON da struct que foi criada
 	responses.JSON(w, http.StatusCreated, loteItensCreated)
-
 }
 
 /*  =========================
@@ -73,6 +92,7 @@ func (server *Server) GetLoteItensByID(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
@@ -100,19 +120,17 @@ func (server *Server) GetLoteItensByID(w http.ResponseWriter, r *http.Request) {
 	loteItens := models.LoteItens{}
 
 	//	loteItensGotten recebe o dado buscado no banco de dados
-	loteItensGotten, err := loteItens.FindLoteItensByID(server.DB, codLote, codItem, codTipoItem)
-
+	loteItensGotten, err := loteItens.FindLoteItensByID(server.DB, uint32(codLote), uint32(codItem), uint32(codTipoItem))
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't find by ID, %v\n", err))
 		return
 	}
 
 	responses.JSON(w, http.StatusOK, loteItensGotten)
-
 }
 
 /*  =========================
-	FUNCAO LISTAR TODA LOTE ITENS
+	FUNCAO LISTAR TODAS LOTE ITENS
 =========================  */
 
 func (server *Server) GetAllLoteItens(w http.ResponseWriter, r *http.Request) {
@@ -133,6 +151,7 @@ func (server *Server) GetAllLoteItens(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't find in database, %v\n", formattedError))
 		return
 	}
+
 	responses.JSON(w, http.StatusOK, allLoteItens)
 }
 
@@ -179,7 +198,16 @@ func (server *Server) UpdateLoteItens(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	loteItens := models.LoteItens{}
+	logLoteItens := models.Log{}
+
 	err = json.Unmarshal(body, &loteItens)
 	if err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
@@ -192,7 +220,15 @@ func (server *Server) UpdateLoteItens(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	updateLoteItens, err := loteItens.UpdateLoteItens(server.DB, codLote, codItem, codTipoItem)
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logLoteItens.LogLoteItens(server.DB, uint32(codLote), uint32(codItem), uint32(codTipoItem), "lote_itens", "u", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
+	updateLoteItens, err := loteItens.UpdateLoteItens(server.DB, uint32(codLote), uint32(codItem), uint32(codTipoItem))
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't update in database , %v\n", formattedError))
@@ -216,8 +252,6 @@ func (server *Server) DeleteLoteItens(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 
-	loteItens := models.LoteItens{}
-
 	//	codLote armazena a chave primaria da tabela lote itens
 	codLote, err := strconv.ParseUint(vars["cod_lote"], 10, 64)
 	if err != nil {
@@ -239,7 +273,25 @@ func (server *Server) DeleteLoteItens(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = loteItens.DeleteLoteItens(server.DB, codLote, codItem, codTipoItem)
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	loteItens := models.LoteItens{}
+	logLoteItens := models.Log{}
+
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logLoteItens.LogLoteItens(server.DB, uint32(codLote), uint32(codItem), uint32(codTipoItem), "lote_itens", "d", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
+	err = loteItens.DeleteLoteItens(server.DB, uint32(codLote), uint32(codItem), uint32(codTipoItem))
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't delete in database , %v\n", formattedError))

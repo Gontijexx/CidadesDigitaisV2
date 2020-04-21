bf82d509bf4413252c7419843ecf1e6a1808860c
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-04-21 09:13:03 -0300

    Processo: implementada a funcao de log .

--- api/control/processo_control.go
+++ api/control/processo_control.go
@@ -1,11 +1,13 @@
 package control
 
 import (
+	"CidadesDigitaisV2/api/auth"
 	"CidadesDigitaisV2/api/config"
 	"CidadesDigitaisV2/api/models"
 	"CidadesDigitaisV2/api/responses"
 	"CidadesDigitaisV2/api/validation"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -22,11 +24,11 @@ import (
 func (server *Server) CreateProcesso(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13091)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13091); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	O metodo ReadAll le toda a request ate encontrar algum erro, se nao encontrar erro o leitura para em EOF
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -34,29 +36,39 @@ func (server *Server) CreateProcesso(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//	Estrutura models.Processo{} "renomeada"
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	processo := models.Processo{}
+	logProcesso := models.Log{}
 
 	//	Unmarshal analisa o JSON recebido e armazena na struct processo referenciada (&struct)
-	err = json.Unmarshal(body, &processo)
-
-	//	Se ocorrer algum tipo de erro retorna-se o Status 422 mais o erro ocorrido
-	if err != nil {
+	if err = json.Unmarshal(body, &processo); err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
 		return
 	}
 
+	//	Validacao de estrutura
 	if err = validation.Validator.Struct(processo); err != nil {
 		log.Printf("[WARN] invalid information, because, %v\n", fmt.Errorf("[FATAL] validation error!, %v\n", err))
 		w.WriteHeader(http.StatusPreconditionFailed)
 		return
 	}
 
+	//	Parametros de entrada(nome_server, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logProcesso.LogProcesso(server.DB, processo.CodProcesso, processo.CodIbge, "processo", "i", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
 	//	SaveProcesso eh o metodo que faz a conexao com banco de dados e salva os dados recebidos
 	processoCreated, err := processo.SaveProcesso(server.DB)
-
-	/*	Retorna um erro caso nao seja possivel salvar processo no banco de dados
-		Status 500	*/
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save in database, %v\n", formattedError))
@@ -67,7 +79,6 @@ func (server *Server) CreateProcesso(w http.ResponseWriter, r *http.Request) {
 
 	//	Ao final retorna o Status 201 e o JSON da struct que foi criada
 	responses.JSON(w, http.StatusCreated, processoCreated)
-
 }
 
 /*  =========================
@@ -77,20 +88,16 @@ func (server *Server) CreateProcesso(w http.ResponseWriter, r *http.Request) {
 func (server *Server) GetProcessoByID(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13092)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13092); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
 	//	codProcesso armazena a chave primaria da tabela processo
-	codProcesso, err := strconv.ParseUint(vars["cod_processo"], 10, 64)
-	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
-		return
-	}
+	codProcesso := vars["cod_processo"]
 
 	//	codIbge armazena a chave primaria da tabela processo
 	codIbge, err := strconv.ParseUint(vars["cod_ibge"], 10, 64)
@@ -102,8 +109,7 @@ func (server *Server) GetProcessoByID(w http.ResponseWriter, r *http.Request) {
 	processo := models.Processo{}
 
 	//	processoGotten recebe o dado buscado no banco de dados
-	processoGotten, err := processo.FindProcessoByID(server.DB, codProcesso, codIbge)
-
+	processoGotten, err := processo.FindProcessoByID(server.DB, codProcesso, uint32(codIbge))
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't find by ID, %v\n", err))
 		return
@@ -111,7 +117,6 @@ func (server *Server) GetProcessoByID(w http.ResponseWriter, r *http.Request) {
 
 	//	Retorna o Status 200 e o JSON da struct buscada
 	responses.JSON(w, http.StatusOK, processoGotten)
-
 }
 
 /*  =========================
@@ -121,11 +126,11 @@ func (server *Server) GetProcessoByID(w http.ResponseWriter, r *http.Request) {
 func (server *Server) GetAllProcesso(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13092)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13092); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	processo := models.Processo{}
 
 	//	allProcesso armazena os dados buscados no banco de dados
@@ -147,20 +152,16 @@ func (server *Server) GetAllProcesso(w http.ResponseWriter, r *http.Request) {
 func (server *Server) UpdateProcesso(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13093)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13093); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
 	//	codProcesso armazena a chave primaria da tabela processo
-	codProcesso, err := strconv.ParseUint(vars["cod_processo"], 10, 64)
-	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
-		return
-	}
+	codProcesso := vars["cod_processo"]
 
 	//	codIbge armazena a chave primaria da tabela processo
 	codIbge, err := strconv.ParseUint(vars["cod_ibge"], 10, 64)
@@ -175,10 +176,17 @@ func (server *Server) UpdateProcesso(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	processo := models.Processo{}
-
-	err = json.Unmarshal(body, &processo)
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
 	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	processo := models.Processo{}
+	logProcesso := models.Log{}
+
+	if err = json.Unmarshal(body, &processo); err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
 		return
 	}
@@ -189,8 +197,16 @@ func (server *Server) UpdateProcesso(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//	Parametros de entrada(nome_server, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logProcesso.LogProcesso(server.DB, codProcesso, uint32(codIbge), "processo", "u", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
 	//	updateProcesso recebe a nova processo, a que foi alterada
-	updateProcesso, err := processo.UpdateProcesso(server.DB, codProcesso, codIbge)
+	updateProcesso, err := processo.UpdateProcesso(server.DB, codProcesso, uint32(codIbge))
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't update in database , %v\n", formattedError))
@@ -208,22 +224,26 @@ func (server *Server) UpdateProcesso(w http.ResponseWriter, r *http.Request) {
 func (server *Server) DeleteProcesso(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo, apenas quem tem permicao de edit pode deletar
-	err := config.AuthMod(w, r, 13093)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13093); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	// Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
-	processo := models.Processo{}
-
-	//	codProcesso armazena a chave primaria da tabela processo
-	codProcesso, err := strconv.ParseUint(vars["cod_processo"], 10, 64)
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
 	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
 		return
 	}
+
+	processo := models.Processo{}
+	logProcesso := models.Log{}
+
+	//	codProcesso armazena a chave primaria da tabela processo
+	codProcesso := vars["cod_processo"]
 
 	//	codIbge armazena a chave primaria da tabela processo
 	codIbge, err := strconv.ParseUint(vars["cod_ibge"], 10, 64)
@@ -232,9 +252,16 @@ func (server *Server) DeleteProcesso(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	/* 	Para o caso da funcao 'delete' apenas o erro nos eh necessario
-	Caso nao seja possivel deletar o dado especificado tratamos o erro*/
-	_, err = processo.DeleteProcesso(server.DB, codProcesso, codIbge)
+	//	Parametros de entrada(nome_server, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logProcesso.LogProcesso(server.DB, codProcesso, uint32(codIbge), "processo", "d", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
+	// 	Para o caso da funcao 'delete' apenas o erro nos eh necessario
+	err = processo.DeleteProcesso(server.DB, codProcesso, uint32(codIbge))
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't delete in database , %v\n", formattedError))

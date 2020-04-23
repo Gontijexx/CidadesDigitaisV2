d9ae34c4cdca31687e264e6d05e35d251ae52ad5
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-04-23 17:28:13 -0300

    Uacom: implementada a funcao de log.

--- api/control/uacom_control.go
+++ api/control/uacom_control.go
@@ -1,21 +1,23 @@
 package control
 
 import (
+	"CidadesDigitaisV2/api/auth"
 	"CidadesDigitaisV2/api/config"
 	"CidadesDigitaisV2/api/models"
 	"CidadesDigitaisV2/api/responses"
 	"CidadesDigitaisV2/api/validation"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-//time.Now().Format(time.RFC3339)
 /*  =========================
 	FUNCAO ADICIONAR UACOM
 =========================  */
@@ -23,11 +25,11 @@ import (
 func (server *Server) CreateUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13101)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13101); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	O metodo ReadAll le toda a request ate encontrar algum erro, se nao encontrar erro o leitura para em EOF
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -35,29 +37,41 @@ func (server *Server) CreateUacom(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//	Estrutura models.Uacom{} "renomeada"
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	uacom := models.Uacom{}
+	logUacom := models.Log{}
+
+	uacom.Data = time.Now().Format(time.RFC3339)
 
 	//	Unmarshal analisa o JSON recebido e armazena na struct uacom referenciada (&struct)
-	err = json.Unmarshal(body, &uacom)
-
-	//	Se ocorrer algum tipo de erro retorna-se o Status 422 mais o erro ocorrido
-	if err != nil {
+	if err = json.Unmarshal(body, &uacom); err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
 		return
 	}
 
+	//	Validacao de estrutura
 	if err = validation.Validator.Struct(uacom); err != nil {
 		log.Printf("[WARN] invalid information, because, %v\n", fmt.Errorf("[FATAL] validation error!, %v\n", err))
 		w.WriteHeader(http.StatusPreconditionFailed)
 		return
 	}
 
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logUacom.LogUacom(server.DB, uacom.CodIbge, uacom.Data, "uacom", "i", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
 	//	SaveUacom eh o metodo que faz a conexao com banco de dados e salva os dados recebidos
 	uacomCreated, err := uacom.SaveUacom(server.DB)
-
-	/*	Retorna um erro caso nao seja possivel salvar uacom no banco de dados
-		Status 500	*/
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save in database, %v\n", formattedError))
@@ -68,7 +82,6 @@ func (server *Server) CreateUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	Ao final retorna o Status 201 e o JSON da struct que foi criada
 	responses.JSON(w, http.StatusCreated, uacomCreated)
-
 }
 
 /*  =========================
@@ -78,11 +91,11 @@ func (server *Server) CreateUacom(w http.ResponseWriter, r *http.Request) {
 func (server *Server) GetUacomByID(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13102)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13102); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
@@ -95,18 +108,11 @@ func (server *Server) GetUacomByID(w http.ResponseWriter, r *http.Request) {
 
 	//	data armazena a chave primaria da tabela uacom
 	data := vars["data"]
-	fmt.Print(data)
-
-	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
-		return
-	}
 
 	uacom := models.Uacom{}
 
 	//	uacomGotten recebe o dado buscado no banco de dados
-	uacomGotten, err := uacom.FindUacomByID(server.DB, codIbge, data)
-
+	uacomGotten, err := uacom.FindUacomByID(server.DB, uint32(codIbge), data)
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't find by ID, %v\n", err))
 		return
@@ -114,7 +120,6 @@ func (server *Server) GetUacomByID(w http.ResponseWriter, r *http.Request) {
 
 	//	Retorna o Status 200 e o JSON da struct buscada
 	responses.JSON(w, http.StatusOK, uacomGotten)
-
 }
 
 /*  =========================
@@ -124,11 +129,11 @@ func (server *Server) GetUacomByID(w http.ResponseWriter, r *http.Request) {
 func (server *Server) GetAllUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13102)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13102); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	uacom := models.Uacom{}
 
 	//	allUacom armazena os dados buscados no banco de dados
@@ -150,11 +155,11 @@ func (server *Server) GetAllUacom(w http.ResponseWriter, r *http.Request) {
 func (server *Server) UpdateUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	Autorizacao de Modulo
-	err := config.AuthMod(w, r, 13103)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13103); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	//	Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
@@ -167,10 +172,6 @@ func (server *Server) UpdateUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	data armazena a chave primaria da tabela uacom
 	data := vars["data"]
-	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
-		return
-	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -178,10 +179,17 @@ func (server *Server) UpdateUacom(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	uacom := models.Uacom{}
-
-	err = json.Unmarshal(body, &uacom)
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
 	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	uacom := models.Uacom{}
+	logUacom := models.Log{}
+
+	if err = json.Unmarshal(body, &uacom); err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("[FATAL] ERROR: 422, %v\n", err))
 		return
 	}
@@ -192,8 +200,16 @@ func (server *Server) UpdateUacom(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logUacom.LogUacom(server.DB, uint32(codIbge), data, "uacom", "u", tokenID)
+	if err != nil {
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
+		return
+	}
+
 	//	updateUacom recebe a nova uacom, a que foi alterada
-	updateUacom, err := uacom.UpdateUacom(server.DB, codIbge, data)
+	updateUacom, err := uacom.UpdateUacom(server.DB, uint32(codIbge), data)
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't update in database , %v\n", formattedError))
@@ -210,15 +226,23 @@ func (server *Server) UpdateUacom(w http.ResponseWriter, r *http.Request) {
 
 func (server *Server) DeleteUacom(w http.ResponseWriter, r *http.Request) {
 
-	err := config.AuthMod(w, r, 13103)
-	if err != nil {
+	if err := config.AuthMod(w, r, 13103); err != nil {
 		responses.ERROR(w, http.StatusUnauthorized, fmt.Errorf("[FATAL] Unauthorized"))
 		return
 	}
+
 	// Vars retorna as variaveis de rota
 	vars := mux.Vars(r)
 
+	//	Extrai o cod_usuario do body
+	tokenID, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	uacom := models.Uacom{}
+	logUacom := models.Log{}
 
 	//	codIbge armazena a chave primaria da tabela uacom
 	codIbge, err := strconv.ParseUint(vars["cod_ibge"], 10, 64)
@@ -229,11 +253,16 @@ func (server *Server) DeleteUacom(w http.ResponseWriter, r *http.Request) {
 
 	//	data armazena a chave primaria da tabela uacom
 	data := vars["data"]
+
+	//	Parametros de entrada(nome_server, chave_primaria, chave_primaria, nome_tabela, operacao, id_usuario)
+	err = logUacom.LogUacom(server.DB, uint32(codIbge), data, "uacom", "d", tokenID)
 	if err != nil {
-		responses.ERROR(w, http.StatusBadRequest, fmt.Errorf("[FATAL] It couldn't parse the variable, %v\n", err))
+		formattedError := config.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't save log in database, %v\n", formattedError))
 		return
 	}
-	_, err = uacom.DeleteUacom(server.DB, codIbge, data)
+
+	err = uacom.DeleteUacom(server.DB, uint32(codIbge), data)
 	if err != nil {
 		formattedError := config.FormatError(err.Error())
 		responses.ERROR(w, http.StatusInternalServerError, fmt.Errorf("[FATAL] it couldn't delete in database , %v\n", formattedError))

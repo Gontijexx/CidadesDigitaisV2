63161b86233527eb96062a243acede5c6b653a63
alexandreps1123 <alexandre.ps1123@gmail.com>
2020-04-23 17:44:35 -0300

    UacomAssunto: a struct UacomAssunto foi movida para este arquivo.

--- api/models/uacomAssunto.go
+++ api/models/uacomAssunto.go
@@ -3,7 +3,17 @@ package models
 import "github.com/jinzhu/gorm"
 
 /*  =========================
-	FUNCAO SALVAR UACOM_ASSUNTO NO BANCO DE DADOS
+	STRUCT UACOM ASSUNTO
+=========================  */
+
+type UacomAssunto struct {
+	CodIbge    uint32 `gorm:"primary_key;foreign_key:CodIbge;not null" json:"cod_ibge"`
+	Data       string `gorm:"primary_key;foreign_key:Data;not null" json:"data"`
+	CodAssunto uint32 `gorm:"primary_key;foreign_key:CodAssunto;not null" json:"cod_assunto"`
+}
+
+/*  =========================
+	FUNCAO SALVAR UACOM ASSUNTO
 =========================  */
 
 func (uacomAssunto *UacomAssunto) SaveUacomAssunto(db *gorm.DB) (*UacomAssunto, error) {
@@ -14,5 +24,5 @@ func (uacomAssunto *UacomAssunto) SaveUacomAssunto(db *gorm.DB) (*UacomAssunto, 
 		return &UacomAssunto{}, err
 	}
 
-	return uacomAssunto, nil
+	return uacomAssunto, err
 }

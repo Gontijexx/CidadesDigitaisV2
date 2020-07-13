c83c080e5aad1f7a8590f8434d5b1a0940848db2
dimitrius <dimitrius_cardoso@hotmail.com>
2020-07-12 22:05:38 -0300

    problema de reajuste contornado

--- front/Js/mask.js
+++ front/Js/mask.js
@@ -1,7 +1,9 @@
-// Uma máscara utilizada para campos de porcentagem
-
-$(document).ready(function(){
+// máscara utilizada para varios campos
+function mascara(){
+  $(document).ready(function(){
     $('.percentual').inputmask('999.99%', {reverse: true, numericInput:true, placeholder:"0"});
     $('.data').mask('00/00/0000');
-    // $("#percentual").inputmask("999.99%",{reverse: true,numericInput:true, placeholder:"0"});
+    $('.data2').mask('00/00');
+    // usando id: $("#percentual").inputmask("999.99%",{reverse: true,numericInput:true, placeholder:"0"});
   });
+}
